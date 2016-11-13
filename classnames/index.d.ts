87d74f9c5d3f07a51eabaa989deb8b445b4a89c4
Henning Dieterichs <notify.henning.dieterichs@live.de>
2016-11-13 19:34:10 +0100

    Allow false as value
    `classNames` supports `false` as input that is ignored, `classNames(false, "class1", "class2")` returns `"class1 class2"`.
    This is very handy in connection with `&&`: `classNames(flag && "myClass")`;

--- classnames/index.d.ts
+++ classnames/index.d.ts
@@ -3,10 +3,10 @@
 // Definitions by: Dave Keen <http://www.keendevelopment.ch>, Adi Dahiya <https://github.com/adidahiya>, Jason Killian <https://github.com/JKillian>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare type ClassValue = string | number | ClassDictionary | ClassArray | undefined | null;
+declare type ClassValue = string | number | ClassDictionary | ClassArray | undefined | null | false;
 
 interface ClassDictionary {
-	[id: string]: boolean | undefined | null;
+	[id: string]: boolean | undefined | null | false;
 }
 
 interface ClassArray extends Array<ClassValue> { }
